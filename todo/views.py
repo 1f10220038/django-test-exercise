66ef754c01b77142079e606fa240151dcebaaec9
1F10220038 <s1f102200389@iniad.org>
2023-07-13 14:03:42 +0900

    add change

--- todo/views.py
+++ todo/views.py
@@ -23,7 +23,7 @@ def index(request):
 
     return render(request, 'todo/index.html', context)
 
-
+# detail page
 def detail(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
